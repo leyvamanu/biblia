e46bd7415bb88d499ec0df1fb7d52da5e4e3f710
Manuel <leyvamanu@gmail.com>
2023-03-30 12:30:47 +0200

    colores

---
+++ script/script.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function () {
+    var copy = document.getElementsByClassName('copy');
+    
+    copy.addEventListener('click', function () {
+        alert('copy');
+        // var input = document.getElementById('input');
+        // var value = input.value;
+        // var result = document.getElementById('result');
+        // result.innerHTML = value;
+    });
+}
---
+++ script/tailwind.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'lois-black': '#000000',
+                'lois-white': '#ffffff',
+                'lois-red': '#e63b3c',
+                'lois-dark-pink': '#eb6465',
+                'lois-pink': '#f8c6c6',
+                'lois-light-gray': '#cccccc',
+                'lois-gray': '#999999',
+                'lois-dark-gray': '#333333',
+                'lois-brown': '#603535',
+            },
+        }
+    }
+}
